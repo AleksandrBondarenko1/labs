29945176942c5159e45396131a5ecd9ffc965c58
AleksandrBondarenko1 <91665908+AleksandrBondarenko1@users.noreply.github.com>
2021-12-29 19:13:36 +0300

    laborat.1

---
+++ laba_1/lab_1_2/lab_1_2.cpp
@@ -0,0 +1,121 @@
+﻿//2. Написать программу, которая преобразует одномерный массив(1D) в двумерный(2D)
+//(или наоборот), в соответствии с вариантом.Необходимо оформить в отдельных
+//функциях код следующих действий : 1) инициализация массива; 2) вывод массива; 3)
+//преобразование массива(создание нового массива с другой структурой).Память под
+//массивы выделять динамически и для доступа к элементам использовать указатели.
+//Ввод - вывод данных организовать средствами iostream и iomanip.
+//Полищук Нилищук Алексеевич
+//Вариант 1
+
+
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+void initializeArray(float* arr, int n)
+{
+	if (n > 0)
+	{
+		*arr = 1.0;
+	}
+
+	if (n > 1)
+	{
+		*(arr + 1) = 1.0;
+	}
+
+	for (int i = 0;i < n;i++)
+	{
+		*(arr + i) = i * sin(M_PI * i / 25);
+	}
+}
+
+
+
+float** makeArray2D(float* arr1D, int a, int b)
+{
+	float** arr2D = new float* [a];
+
+	for (int i = 0; i < a;++i)
+	{
+		*(arr2D + i) = new float[b];
+	}
+	for (int i = 0;i < a;++i)
+	{
+		for (int j = 0;j < b;++j)
+		{
+			*(*(arr2D + i) + j) = *(arr1D + i * b + j);
+		}
+	}
+	return arr2D;
+}
+
+void multiplicationFirstElement(float** arr, int N, int M)
+{
+	for (int i = 0;i < N;i++)
+	{
+		float sum=0;
+		for (int j = 1;j < M;j++)
+		{
+
+			sum += arr[i][j];
+			
+		}
+		arr[i][0] = sum;
+	}
+}
+
+void printArray1D(float arr[], int n)
+{
+	for (int i = 0; i < n;++i)
+	{
+		printf("%f"" ", arr[i]);
+	}
+	
+}
+
+void printArray2D(float** arr, int N, int M)
+{
+	for (int i = 0;i < N;i++)
+	{
+		
+		for (int j = 0;j < M;j++)
+		{
+			printf("%f"" ", arr[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void freeArray1D(float arr[], int len)
+{
+	delete[] arr;
+}
+
+void freeArray2D(float** arr)
+{
+	delete[] arr;
+}
+
+	int main()
+	{
+		const int LEN = 25; //задаем параметры массивов
+		const int N = 5;
+		const int M = 5;
+
+		float* arr = new float[LEN]; //выделяем память под массив
+
+		initializeArray(arr, LEN);// Инициализируем массив
+
+		printArray1D(arr, LEN); //вывод в консоль массива 1D
+		cout << "\n";
+		cout << "\n";
+		//конвертируем 1 мерный массив в 2 мерный
+		float** arr2D = makeArray2D(arr, N, M);
+		multiplicationFirstElement(arr2D, N, M);
+		printArray2D(arr2D, N, M);
+		freeArray1D(arr, LEN);
+		freeArray2D(arr2D);
+	}
---
+++ laba_1/lab_1_3/lab_1_3.cpp
@@ -0,0 +1,42 @@
+﻿//3. Написать свой аналог стандартной функции обработки строк из файла cstring, в
+//соответствии с вариантом.В функции main на тестовых данных продемонстрировать
+//результат работы как стандартной функции, так и собственной версии.Ввод - вывод
+//данных организовать средствами cstdio
+//Полищук Нилищук Алексеевич
+//Вариант 1
+#include <iostream>
+
+char* reverseChar(char* s, int c)
+{
+	int len = 0;
+
+	char* rest_of_string = NULL;
+
+	while (s[len] != '\0')
+	{
+		
+		if (s[len] == c)
+		{
+			rest_of_string = s + len;
+			break; //Выход изи цикла после первого найденного совпадения 
+		}
+		len++;
+	}
+
+	return rest_of_string;
+}
+
+int main()
+{
+	char s[] = "www.some_address.add_something_else.com";
+
+	char* standard_result = strchr(s, '.');
+
+	char* my_result = reverseChar(s, '.');
+
+	printf("Initial string: %s\nStandard: %s\nMy version: %s\n", s, standard_result, my_result);
+
+	return 0;
+
+} 
+
